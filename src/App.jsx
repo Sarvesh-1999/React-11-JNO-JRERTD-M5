24e3f2b6f12847e0d71ca25ff53a4f189c8616f6
Sarvesh <>
2026-02-26 12:34:41 +0530

    states

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import FunctionBased from "./AllTopics/01_TypesOfComponents/FunctionBased";
 import ClassBased from "./AllTopics/01_TypesOfComponents/ClassBased";
 import StatesInFBC from "./AllTopics/02_States/StatesInFBC";
+import ThemeChange from "./AllTopics/02_States/ThemeChange";
+import Counter from "./AllTopics/02_States/Counter";
 
 const App = () => {
   return (
     <>
       {/* <FunctionBased /> */}
       {/* <ClassBased /> */}
-      <StatesInFBC />
+      {/* <StatesInFBC /> */}
+      {/* <ThemeChange /> */}
+      <Counter />
     </>
   );
 };
